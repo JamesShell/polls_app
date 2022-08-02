fcbc964b99d3a7060b1069f86379f90a0cce0417
SoProffessional <so.professional.me@gmail.com>
2022-08-02 21:07:19 +0100

    Project utils & Globlal Variables

---
+++ lib/utils/_global_variables.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../screens/feed.dart';
+
+const webScreenSize = 600;
+const homeScreensItems = [
+  FeedScreen(),
+];
--- lib/utils/pallete.dart
+++ lib/utils/pallete.dart
@@ -4,4 +4,4 @@ import 'package:flutter/cupertino.dart' as cuper;
 const TextStyle kBodyText = TextStyle(fontSize: 16, color: Colors.white);
 
 const Color kWhite = Colors.white;
-const Color kBlue = cuper.Color(0xff5663ff);
+const Color kBlue = cuper.Color(0xff014666);
